f4ba677d59c24ade81db9fd91274dcf777496809
Fatih <fatihcan.social@gmail.com>
2020-06-15 20:58:50 +0300

    change sign up sign in buttons

--- components/landing-nav.js
+++ components/landing-nav.js
@@ -5,9 +5,20 @@ import NavItem from './nav-item'
 
 const LandingNav = () => (
   <Header>
-    <Stack row>
-      <NavItem href="/sign-in">Sign In</NavItem>
-      <NavItem href="/sign-up">Sign Up</NavItem>
+    <Stack row gap={16}>
+      <NavItem
+        href="/sign-in"
+        className="py-2 px-3 border border-blue-500 rounded-md transition-colors duration-300 text-blue-500
+        hover:bg-blue-500 hover:text-white"
+      >
+        Sign In
+      </NavItem>
+      <NavItem
+        href="/sign-up"
+        className="py-2 px-3 bg-blue-500 rounded-md transition-colors duration-300 hover:bg-blue-700"
+      >
+        Sign Up
+      </NavItem>
       <NavItem href="/about">About</NavItem>
     </Stack>
   </Header>
