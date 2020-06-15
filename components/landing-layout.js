54b4a9efa8c7182c04e1affe64e66f523326e0b3
Fatih <fatihcan.social@gmail.com>
2020-06-15 21:05:50 +0300

    add seo component

--- components/landing-layout.js
+++ components/landing-layout.js
@@ -2,12 +2,16 @@ import LandingNav from './landing-nav'
 import cd from 'classnames'
 import layoutStyles from '../styles/landing-layout.module.css'
 import mainStyles from '../styles/main.module.css'
+import Seo from './seo'
 
 const LandingLayout = ({ children }) => (
-  <div className={cd('min-h-screen flex flex-col', layoutStyles.layout)}>
-    <LandingNav />
-    <main className={mainStyles.main}>{children}</main>
-  </div>
+  <>
+    <Seo title="guidemus | Find the best guide for your Erasmus journey." />
+    <div className={cd('min-h-screen flex flex-col', layoutStyles.layout)}>
+      <LandingNav />
+      <main className={mainStyles.main}>{children}</main>
+    </div>
+  </>
 )
 
 export default LandingLayout
