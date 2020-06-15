d2d94abdf592a278648f65b02677e25c5388a3cc
Fatih <fatihcan.social@gmail.com>
2020-06-15 21:05:44 +0300

    create seo component

---
+++ components/seo.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+
+const Seo = ({ title }) => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>{title}</title>
+      <link rel="canonical" href="https://guidemus-nextjs.vercel.app/" />
+      <meta
+        name="description"
+        content="A platform for the students to find the best guide for their Erasmus journey."
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://guidemus-nextjs.vercel.app/" />
+      <meta property="og:title" content="Pomodoro Timer" />
+      <meta
+        property="og:description"
+        content="A platform for the students to find the best guide for their Erasmus journey."
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://guidemus-nextjs.vercel.app/"
+      />
+      <meta property="twitter:title" content="Pomodoro Timer" />
+      <meta
+        property="twitter:description"
+        content="A platform for the students to find the best guide for their Erasmus journey."
+      />
+    </Head>
+  )
+}
+
+export default Seo
