92fc890ca62357ea3c341cafa47d01974058681e
Fatih <fatihcan.social@gmail.com>
2020-06-16 11:40:12 +0300

    add z-index to itself and children

--- components/landing-layout.js
+++ components/landing-layout.js
@@ -1,16 +1,26 @@
 import LandingNav from './landing-nav'
-import cd from 'classnames'
-import layoutStyles from '../styles/landing-layout.module.css'
+import cn from 'classnames'
+import layoutStyles from '../styles/landing-layout.module.scss'
 import mainStyles from '../styles/main.module.css'
 import Seo from './seo'
+import Image from './image'
+
+const landing = require('../public/landing.jpg?lqip')
 
 const LandingLayout = ({ children }) => (
   <>
     <Seo title="guidemus | Find the best guide for your Erasmus journey." />
-    <div className={cd('min-h-screen flex flex-col', layoutStyles.layout)}>
+    <Image
+      src={landing.src}
+      alt="directions"
+      className="fixed min-h-screen object-cover top-0 bottom-0 right-0 left-0 z-0"
+    />
+    <section className={cn('min-h-screen flex flex-col', layoutStyles.layout)}>
       <LandingNav />
-      <main className={mainStyles.main}>{children}</main>
-    </div>
+      <main className={cn('flex flex-col justify-center p-4', mainStyles.main)}>
+        {children}
+      </main>
+    </section>
   </>
 )
 
