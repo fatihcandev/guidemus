502aa1af7d8b5a5ef5f3a41497e074d4e520ad6b
Fatih <fatihcan.social@gmail.com>
2020-06-16 12:24:16 +0300

    add aria-label

--- components/input.js
+++ components/input.js
@@ -1,9 +1,10 @@
 import cd from 'classnames'
 
-const Input = ({ className, ph, ...props }) => (
+const Input = ({ className, ph, label, ...props }) => (
   <input
     className={cd('rounded-md focus:outline-none', className)}
     placeholder={ph}
+    aria-label={label}
     {...props}
   />
 )
