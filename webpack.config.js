3dfa429bab0c32718de5e09f388f78a6a712701f
Fatih <fatihcan.social@gmail.com>
2020-06-16 11:40:39 +0300

    lqip loader config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  test: /\.(jpe?g|png)$/i,
+  test: /\.jpe?g$/,
   loaders: [
     {
       loader: 'lqip-loader',
       options: {
-        base64: true,
-        palette: false
+        path: '/', // your image going to be in media folder in the output dir
+        name: '[name].[ext]', // you can use [hash].[ext] too if you wish,
+        base64: true // default: true, gives the base64 encoded image
       }
     }
   ]
