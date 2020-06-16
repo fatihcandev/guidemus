a3ec5792712ccbffafa1e6c6574141c268ec54a1
Fatih <fatihcan.social@gmail.com>
2020-06-16 15:19:18 +0300

    firebase config

---
+++ firebase/config.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAjLGO47X-BlrV4E2e8J08iSu5ANPQVqeI',
+  authDomain: 'guidemus.firebaseapp.com',
+  databaseURL: 'https://guidemus.firebaseio.com',
+  projectId: 'guidemus',
+  storageBucket: 'guidemus.appspot.com',
+  messagingSenderId: '589695062330',
+  appId: '1:589695062330:web:009704904645aa1ff78003',
+  measurementId: 'G-WZC8E2454S'
+}
+
+firebase.initializeApp(firebaseConfig)
+export const auth = firebase.auth()
+export const db = firebase.firestore()
