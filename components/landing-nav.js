37f00f21a8049105871882a8267d3669897e29e9
Fatih <fatihcan.social@gmail.com>
2020-06-16 12:25:49 +0300

    add Nav

--- components/landing-nav.js
+++ components/landing-nav.js
@@ -2,25 +2,28 @@ import Header from './header'
 import TextBody from './text-body'
 import Stack from './stack'
 import NavItem from './nav-item'
+import Nav from './nav'
 
 const LandingNav = () => (
-  <Header>
-    <Stack row gap={16}>
-      <NavItem
-        href="/sign-in"
-        className="py-2 px-3 border border-blue-500 rounded-md transition-colors duration-300 text-blue-500
+  <Header className="justify-end">
+    <Nav>
+      <Stack row gap={16}>
+        <NavItem
+          href="/sign-in"
+          className="py-2 px-3 border border-blue-500 rounded-md transition-colors duration-300 text-blue-500
         hover:bg-blue-500 hover:text-white"
-      >
-        Sign In
-      </NavItem>
-      <NavItem
-        href="/sign-up"
-        className="py-2 px-3 bg-blue-500 rounded-md transition-colors duration-300 hover:bg-blue-700"
-      >
-        Sign Up
-      </NavItem>
-      <NavItem href="/about">About</NavItem>
-    </Stack>
+        >
+          Sign In
+        </NavItem>
+        <NavItem
+          href="/sign-up"
+          className="py-2 px-3 bg-blue-500 rounded-md transition-colors duration-300 hover:bg-blue-700"
+        >
+          Sign Up
+        </NavItem>
+        <NavItem href="/about">About</NavItem>
+      </Stack>
+    </Nav>
   </Header>
 )
 
