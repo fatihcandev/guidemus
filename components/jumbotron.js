cb73cfe5f2080c0211da4b9cbcada26878dd2d69
Fatih <fatihcan.social@gmail.com>
2020-06-16 11:40:25 +0300

    create jumbotron

---
+++ components/jumbotron.js
@@ -0,0 +1,14 @@
+import TextTitle from './text-title'
+
+const Jumbotron = () => (
+  <div className="mb-4">
+    <TextTitle className="text-2xl font-bold text-center text-white sm:text-3xl lg:text-4xl">
+      The world is one click away
+    </TextTitle>
+    <TextTitle className="text-center text-white">
+      Find the best guide for your Erasmus journey
+    </TextTitle>
+  </div>
+)
+
+export default Jumbotron
