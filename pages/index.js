a2a8568f6b8d312a3bf3ac49c9324468dc910c09
Fatih <fatihcan.social@gmail.com>
2020-06-16 11:41:31 +0300

    add jumbotron

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import LandingLayout from '../components/landing-layout'
 import LandingSearch from '../components/landing-search'
+import Jumbotron from '../components/jumbotron'
 
 const Index = () => (
   <LandingLayout>
+    <Jumbotron />
     <LandingSearch />
   </LandingLayout>
 )
