0263db392db1515c0768415e8378724e506ba691
Fatih <fatihcan.social@gmail.com>
2020-06-17 13:30:44 +0300

    set  width and height of loading spinner

--- components/spinner.js
+++ components/spinner.js
@@ -1,10 +1,8 @@
 const Spinner = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink="http://www.w3.org/1999/xlink"
-    style="margin: auto; background: none; display: block; shape-rendering: auto;"
-    width="200px"
-    height="200px"
+    width={25}
+    height={25}
     viewBox="0 0 100 100"
     preserveAspectRatio="xMidYMid"
   >
@@ -13,16 +11,16 @@ const Spinner = () => (
       cy="50"
       fill="none"
       stroke="#ffffff"
-      stroke-width="10"
+      strokeWidth="10"
       r="35"
-      stroke-dasharray="164.93361431346415 56.97787143782138"
+      strokeDasharray="164.93361431346415 56.97787143782138"
       transform="rotate(85.1638 50 50)"
     >
       <animateTransform
         attributeName="transform"
         type="rotate"
         repeatCount="indefinite"
-        dur="1s"
+        dur="0.3s"
         values="0 50 50;360 50 50"
         keyTimes="0;1"
       ></animateTransform>
