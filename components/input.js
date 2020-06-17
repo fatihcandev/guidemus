d260544e93a3b509d9144f5d1962a98447819d4f
Fatih <fatihcan.social@gmail.com>
2020-06-17 13:32:33 +0300

    get state and dispatch with redux

--- components/input.js
+++ components/input.js
@@ -1,10 +1,16 @@
 import cn from 'classnames'
 
-const Input = ({ className, ph, label, ...props }) => (
+const Input = ({ password, ph, onChange, value, className, ...props }) => (
   <input
-    className={cn('rounded-md focus:outline-none', className)}
+    type={password ? 'password' : 'text'}
+    className={cn(
+      'rounded-md bg-gray-100 focus:outline-none focus:bg-white py-3',
+      className
+    )}
     placeholder={ph}
-    aria-label={label}
+    aria-label={ph}
+    onChange={(e) => onChange(e.target.value)}
+    value={value}
     {...props}
   />
 )
