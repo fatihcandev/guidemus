20c2d8eb7f649e33c5ae59fb05f96425070923ad
Fatih <fatihcan.social@gmail.com>
2020-06-18 13:17:13 +0300

    add authentication and firestore

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,13 +1,37 @@
-import { auth } from './config'
+import { auth, db } from './config'
 
-export const signUp = async (email, pass) => {
+export const signUp = async (name, email, pass) => {
   try {
     if (email === '' || pass === '') {
       return 'Email and password is required'
     } else {
-      const response = await auth.signInWithEmailAndPassword(email, pass)
-      return response.user
+      const response = await auth.createUserWithEmailAndPassword(email, pass)
+      const user = response.user
+      if (user) {
+        await verifyEmail(user)
+      }
     }
+  } catch (e) {
+    return e.message
+  }
+}
+
+const addToFirestore = async (uid, name, email, pass) => {
+  try {
+    return await db.collection('users').doc(uid).set({
+      uid: uid,
+      name: name,
+      email: email,
+      password: pass
+    })
+  } catch (e) {
+    return e.message
+  }
+}
+
+const verifyEmail = async (user) => {
+  try {
+    await user.sendEmailVerification()
   } catch (e) {
     return e.message
   }
