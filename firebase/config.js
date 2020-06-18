c070e3a90ae3d3f99ec694ef136a253448c045c0
Fatih <fatihcan.social@gmail.com>
2020-06-18 13:17:22 +0300

    export firestore method

--- firebase/config.js
+++ firebase/config.js
@@ -13,4 +13,4 @@ const config = {
 
 !firebase.apps.length && firebase.initializeApp(config)
 export const auth = firebase.auth()
-export const db = firebase.database()
+export const db = firebase.firestore()
