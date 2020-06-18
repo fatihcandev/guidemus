ed19331a0345a6a916dc81ff76e9539ec099bff5
Fatih <fatihcan.social@gmail.com>
2020-06-18 13:21:21 +0300

    import elements as a component

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,44 +1,19 @@
-import signUpImg from '../public/ball-shaped-blur-close-up-focus-346885.jpg?webp'
-import Image from '../components/image'
 import { initializeStore } from '../redux/store'
-import { EmailInput, FormContainer } from '../redux/containers/email-input'
-import { PasswordInput } from '../redux/containers/pass-input'
-import TextBody from '../components/text-body'
-import Stack from '../components/stack'
-import SocialSignInButton from '../components/social-sign-in-button'
-import NavItem from '../components/nav-item'
-import { lazyLoad } from '../lazy-load/lazy-load-image'
-import { useEffect } from 'react'
+import { SignUpFormContainer } from '../redux/containers/sign-up-form'
+import { useState } from 'react'
+import SignUpImage from '../components/sign-up-image'
 
 const SignUp = () => {
-  useEffect(() => {
-    lazyLoad()
-  })
+  const [isSubmitting, setSubmitting] = useState(false)
+  const [isEmailVerificationSent, setEmailVerificationSent] = useState(false)
 
   return (
     <div className="min-h-screen flex">
-      <div className="hidden md:flex md:w-1/4 lg:w-1/3 xl:w-1/2">
-        <Image
-          src={signUpImg}
-          dataSrc={signUpImg.src}
-          className="object-cover"
-        />
-      </div>
-      <div className="p-4 flex flex-col justify-center mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
-        <FormContainer>
-          <SocialSignInButton platform="google" />
-          <SocialSignInButton platform="facebook" />
-          <TextBody className="mx-auto">OR</TextBody>
-          <EmailInput ph="E-mail address" className="border py-2 pl-2" />
-          <PasswordInput password ph="Password" className="border py-2 pl-2" />
-          <Stack row gap={3} className="justify-center">
-            <TextBody>Have an account?</TextBody>
-            <NavItem className="font-bold hover:underline" href="/sign-in">
-              Sign In
-            </NavItem>
-          </Stack>
-        </FormContainer>
-      </div>
+      <SignUpImage />
+      <SignUpFormContainer
+        isSubmitting={isSubmitting}
+        setSubmitting={setSubmitting}
+      />
     </div>
   )
 }
