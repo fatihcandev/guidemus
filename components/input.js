0f605e96b767b1f9df6c668014482a017238ca08
Fatih <fatihcan.social@gmail.com>
2020-06-18 19:22:27 +0300

    add props for formik and return formik field component

--- components/input.js
+++ components/input.js
@@ -1,16 +1,26 @@
 import cn from 'classnames'
+import { Field } from 'formik'
 
-const Input = ({ password, ph, onChange, value, className, ...props }) => (
-  <input
-    type={password ? 'password' : 'text'}
+const Input = ({
+  name,
+  type,
+  ph,
+  onChange,
+  value,
+  validationFunc,
+  className,
+  ...props
+}) => (
+  <Field
+    type={type}
+    name={name}
     className={cn(
       'rounded-md bg-gray-100 focus:outline-none focus:bg-white py-3',
       className
     )}
     placeholder={ph}
+    validate={validationFunc}
     aria-label={ph}
-    onChange={(e) => onChange(e.target.value)}
-    value={value}
     {...props}
   />
 )
