835e1bfec50c266203f65c709c351c91114be1f2
Fatih <fatihcan.social@gmail.com>
2020-06-18 20:05:00 +0300

    comment the redux imports for later use

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
 import '../styles/app.css'
 import '../styles/style.css'
-import { Provider } from 'react-redux'
-import { useStore } from '../redux/store'
+/*import { Provider } from 'react-redux'
+import { useStore } from '../redux/store'*/
 
 const App = ({ Component, pageProps }) => {
-  /*  const store = useStore(pageProps.initialReduxState)*/
   return <Component {...pageProps} />
 }
 
