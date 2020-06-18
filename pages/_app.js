18fb8d7dc8c35cdc4341b681bb56cf6c3c731c03
Fatih <fatihcan.social@gmail.com>
2020-06-18 19:21:44 +0300

    remove redux integration until the next usage

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,9 @@ import '../styles/style.css'
 import { Provider } from 'react-redux'
 import { useStore } from '../redux/store'
 
-export default function App({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
-
-  return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
-  )
+const App = ({ Component, pageProps }) => {
+  /*  const store = useStore(pageProps.initialReduxState)*/
+  return <Component {...pageProps} />
 }
+
+export default App
