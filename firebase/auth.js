6a1bae31985bee1106ad4a07c1cbae5ca6977d74
Fatih <fatihcan.social@gmail.com>
2020-06-18 13:52:47 +0300

    add email verification

--- firebase/auth.js
+++ firebase/auth.js
@@ -1,14 +1,17 @@
 import { auth, db } from './config'
 
-export const signUp = async (name, email, pass) => {
+export const signUp = async (name, email, pass, setEmailVerificationSent) => {
   try {
     if (email === '' || pass === '') {
       return 'Email and password is required'
     } else {
       const response = await auth.createUserWithEmailAndPassword(email, pass)
       const user = response.user
+      const uid = user.uid
       if (user) {
+        setEmailVerificationSent(true)
         await verifyEmail(user)
+        await addToFirestore(uid, name, email, pass)
       }
     }
   } catch (e) {
@@ -18,7 +21,7 @@ export const signUp = async (name, email, pass) => {
 
 const addToFirestore = async (uid, name, email, pass) => {
   try {
-    return await db.collection('users').doc(uid).set({
+    await db.collection('users').doc(uid).set({
       uid: uid,
       name: name,
       email: email,
