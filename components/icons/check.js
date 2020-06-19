ba90ffe20e8e91ebd743cf1de5667b656e767dfc
Fatih <fatihcan.social@gmail.com>
2020-06-19 17:11:59 +0300

    create check icon component

---
+++ components/icons/check.js
@@ -0,0 +1,15 @@
+const Check = () => (
+  <svg
+    className="w-6 h-6 text-green-500"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+  </svg>
+)
+
+export default Check
