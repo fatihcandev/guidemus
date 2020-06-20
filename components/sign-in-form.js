1ddc8087331b4d6c95d34887c6a8560977c837ea
Fatih <fatihcan.social@gmail.com>
2020-06-20 15:22:25 +0300

    create sign in layout

---
+++ components/sign-in-form.js
@@ -0,0 +1,81 @@
+import FormButton from './form-button'
+import { signIn, signUp } from '../firebase/auth'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import Stack from './stack'
+import TextBody from './text-body'
+import NavItem from './nav-item'
+import SocialSignInButton from './social-sign-in-button'
+import Alert from './alert'
+import { useState } from 'react'
+
+const SignInForm = ({ setEmailVerificationSent }) => {
+  const [error, setError] = useState(null)
+
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={(values) => {
+        const errors = {}
+        if (!values.email) {
+          errors.email = 'Required'
+        } else if (!values.password) {
+          errors.password = 'Required'
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address'
+        }
+        return errors
+      }}
+      onSubmit={async (values, { setSubmitting }) => {
+        const response = await signIn(values.email, values.password)
+        response && setError(response)
+        setSubmitting(false)
+      }}
+    >
+      {({ errors, isSubmitting }) => (
+        <Form>
+          <Stack>
+            <SocialSignInButton platform="google" />
+            <SocialSignInButton platform="facebook" />
+            <TextBody className="mx-auto">OR</TextBody>
+            <Field
+              className={`rounded-md bg-gray-100 focus:outline-none focus:bg-white py-3 border-2 py-2 pl-2 
+            focus:border-blue-500 ${errors.email && 'border-red-500'}`}
+              placeholder="E-mail Address"
+              name="email"
+              type="email"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-red-500 font-bold"
+            />
+            <Field
+              className={`rounded-md bg-gray-100 focus:outline-none focus:bg-white py-3 border-2 py-2 pl-2 
+            focus:border-blue-500 ${errors.password && 'border-red-500'}`}
+              placeholder="Password"
+              name="password"
+              type="password"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-red-500 font-bold"
+            />
+            <FormButton isLoading={isSubmitting}>Sign In</FormButton>
+            <Stack row gap={3} className="justify-center">
+              <TextBody>Don't have an account?</TextBody>
+              <NavItem className="font-bold" href="/sign-up">
+                Sign Up
+              </NavItem>
+            </Stack>
+          </Stack>
+          {error && <Alert type="error" message={error} />}
+        </Form>
+      )}
+    </Formik>
+  )
+}
+
+export default SignInForm
--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,3 +1,16 @@
-const SignIn = () => <div>Sign In</div>
+import SignInForm from '../components/sign-in-form'
+import cn from 'classnames'
+import styles from '../styles/sign-in-img.module.css'
+
+const SignIn = () => (
+  <div className="min-h-screen flex">
+    <div
+      className={cn('hidden md:flex md:w-1/4 lg:w-1/3 xl:w-1/2', styles.img)}
+    ></div>
+    <div className="p-4 flex flex-col justify-center mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
+      <SignInForm />
+    </div>
+  </div>
+)
 
 export default SignIn
