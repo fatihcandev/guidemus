d4ddac2cd3ae7f0b565419f3bf510c40ff8a796b
Fatih <fatihcan.social@gmail.com>
2020-06-20 15:21:59 +0300

    add sign up image as bg

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import SignUpImage from '../components/sign-up-image'
 import VerificationSent from '../components/verification-sent'
 import SignUpForm from '../components/sign-up-form'
+import cn from 'classnames'
+import styles from '../styles/sign-up-img.module.css'
 
 const SignUp = () => {
   const [isEmailVerificationSent, setEmailVerificationSent] = useState(false)
 
   return (
     <div className="min-h-screen flex">
-      <SignUpImage />
+      <div
+        className={cn('hidden md:flex md:w-1/4 lg:w-1/3 xl:w-1/2', styles.img)}
+      ></div>
       <div className="p-4 flex flex-col justify-center mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
         {isEmailVerificationSent ? (
           <VerificationSent />
