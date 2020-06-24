b3f46f93c05b7fd930192e5519869cdc054acb1e
Fatih <fatihcan.social@gmail.com>
2020-06-24 19:49:36 +0300

    close empty div

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -11,7 +11,7 @@ const SignUp = () => {
     <div className="min-h-screen flex">
       <div
         className={cn('hidden md:flex md:w-1/4 lg:w-1/3 xl:w-1/2', styles.img)}
-      ></div>
+      />
       <div className="p-4 flex flex-col justify-center mx-auto w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
         {isEmailVerificationSent ? (
           <VerificationSent />
