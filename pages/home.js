6dbcf274a5c52ecc69a8f38405c46ddd7d83a2f0
Fatih <fatihcan.social@gmail.com>
2020-06-24 19:47:35 +0300

    add getInitialProps method

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,20 @@
-const Home = () => <div>This is home.</div>
+import withPrivateRoute from '../components/withPrivateRoute'
+import { signOut } from '../firebase/auth'
+import { auth } from '../firebase/config'
 
-export default Home
+const Home = () => {
+  return (
+    <div>
+      This is home
+      <button onClick={() => signOut}>Logout</button>
+    </div>
+  )
+}
+
+Home.getInitialProps = async (props) => {
+  const user = auth.currentUser
+  console.log(user === null)
+  return {}
+}
+
+export default withPrivateRoute(Home)
